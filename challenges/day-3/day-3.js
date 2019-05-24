31e07ae1d886c6bb0aaf2628ac05beb5f63b9f1a
Davide Crestini <davide.crestini94@gmail.com>
2019-05-24 13:39:37 +0100

    completed day 3 challenge

--- challenges/day-3/day-3.js
+++ challenges/day-3/day-3.js
@@ -1,10 +1,33 @@
-const productOfOtherNumbers = array => {
-  return array.map((num, i) => {
-    return [...array.slice(0, i), ...array.slice(i + 1)].reduce(
-      (acc, value) => acc * value,
-      1
-    );
-  });
+import Node from './Node';
+
+const serializer = (node, output) => {
+  if (!node) {
+    return output.push('#');
+  }
+
+  output.push(node.val());
+  serializer(node.left(), output);
+  serializer(node.right(), output);
 };
 
-export default productOfOtherNumbers;
+export const serialize = tree => {
+  const vals = [];
+  serializer(tree, vals);
+
+  return vals.join(',');
+};
+
+export const deserialize = string => {
+  const array = string.split(',');
+
+  let i = 0;
+  const decode = () => {
+    if (i >= array.length || array[i] === '#') {
+      return null;
+    }
+
+    return new Node(array[i], decode(++i), decode(++i));
+  };
+
+  return decode();
+};
--- challenges/day-3/day-3.test.js
+++ challenges/day-3/day-3.test.js
@@ -1,10 +1,55 @@
-import productOfOtherNumbers from './day-2';
+import { serialize, deserialize } from './day-3';
+import Node from './Node';
 
-const couples = [
-  [[1, 2, 3, 4, 5], [120, 60, 40, 30, 24]],
-  [[3, 2, 1], [2, 3, 6]],
-];
+describe('day-3 test 1', () => {
+  const node = new Node(
+    'root',
+    new Node('left', new Node('left.left')),
+    new Node('right')
+  );
+  test('serialize-deserialize', () => {
+    const serializedDeserialized = deserialize(serialize(node));
+    // console.log(serializedDeserialized);
+    expect(
+      serializedDeserialized
+        .left()
+        .left()
+        .val()
+    ).toBe('left.left');
 
-test.each(couples)('.productOfOtherNumbers(%i, %i)', (array, expected) => {
-  expect(productOfOtherNumbers(array)).toEqual(expected);
+    expect(serializedDeserialized.right().val()).toBe('right');
+  });
+
+  test('idempotence', () => {
+    const idempotentSerialization = serialize(deserialize(serialize(node)));
+    expect(idempotentSerialization).toBe(serialize(node));
+  });
+});
+
+describe('day-3 test 2', () => {
+  const node = new Node(
+    1,
+    new Node(2, new Node(6)),
+    new Node(3, new Node(4), new Node(5))
+  );
+  test('serialize-deserialize', () => {
+    const serializedDeserialized = deserialize(serialize(node));
+    // console.log(serializedDeserialized);
+    expect(
+      parseInt(
+        serializedDeserialized
+          .left()
+          .left()
+          .val(),
+        10
+      )
+    ).toEqual(6);
+
+    expect(parseInt(serializedDeserialized.right().val(), 10)).toBe(3);
+  });
+
+  test.only('idempotence', () => {
+    const idempotentSerialization = serialize(deserialize(serialize(node)));
+    expect(idempotentSerialization).toBe(serialize(node));
+  });
 });
