122425e1868f2a7e56c34fa482f3257daa3f5f54
Davide Crestini <davide.crestini94@gmail.com>
2019-05-30 10:08:55 +0100

    setup test for day 9

--- challenges/day-6/day-6.test.js
+++ challenges/day-6/day-6.test.js
@@ -1,15 +1,8 @@
-import findFirstMissingpositive from './day-6';
-
-const couples = [
-  [[3, 4, -1, 1], 2],
-  [[1, 2, 0], 3],
-  [[-1, 2, 24, -5, 0, 5, 1], 3],
-  [[1, -1, -5, -3, 3, 4, 2, 8], 5],
-  [[1, 8, 2, 4, 3, -1, -5, -3], 5],
-];
+import XORList from './day-6';
 
 describe('day-6', () => {
-  test.each(couples)('.findFirstMissingpositive', (array, expected) => {
-    expect(findFirstMissingpositive(array)).toEqual(expected);
+  test('XOR List', (array, expected) => {
+    const list = new XORList();
+    expect(list).toEqual(null);
   });
 });
--- challenges/day-9/day-9.test.js
+++ challenges/day-9/day-9.test.js
@@ -1,12 +1,6 @@
 import maxSumDistantNumbers from './day-9';
 
-const couples = [
-  ['111', 3],
-  ['1011', 2],
-  ['01011', 0],
-  ['12020', 1],
-  ['1215647', 21],
-];
+const couples = [[[2, 4, 6, 2, 5], 13], [[5, 1, 1, 5], 10]];
 
 test.each(couples)('.maxSumDistantNumbers', (array, expected) => {
   expect(maxSumDistantNumbers(array)).toEqual(expected);
