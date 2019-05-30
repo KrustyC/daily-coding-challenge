9d3f7035f463a493a5e4233b4e0d686a8c64c5ac
Davide Crestini <davide.crestini94@gmail.com>
2019-05-30 11:01:07 +0100

    solved day 9

--- challenges/day-9/day-9.js
+++ challenges/day-9/day-9.js
@@ -1,5 +1,24 @@
-const maxSumDistantNumbers = array => {
-  return 0;
+const arrayMax = array => {
+  let max = -Infinity;
+
+  array.forEach(x => {
+    if (x > max) {
+      max = x;
+    }
+  });
+
+  return max;
 };
 
-export default maxSumDistantNumbers;
+const largestSumOfNonAdjacent = array => {
+  const { length } = array;
+
+  for (let i = length - 3; i >= 0; i--) {
+    const maxOfNonAdjacent = arrayMax(array.slice(i + 2));
+    array[i] = maxOfNonAdjacent + array[i];
+  }
+
+  return array[0] > array[1] ? array[0] : array[1];
+};
+
+export default largestSumOfNonAdjacent;
--- challenges/day-9/day-9.test.js
+++ challenges/day-9/day-9.test.js
@@ -1,7 +1,12 @@
-import maxSumDistantNumbers from './day-9';
+import largestSumOfNonAdjacent from './day-9';
 
-const couples = [[[2, 4, 6, 2, 5], 13], [[5, 1, 1, 5], 10]];
+const couples = [
+  [[2, 4, 6, 2, 5], 13],
+  [[5, 1, 1, 5], 10],
+  [[5, 5, 10, 40, 50, 35], 80],
+  [[1, 0, 3, 9, 2], 10],
+];
 
-test.each(couples)('.maxSumDistantNumbers', (array, expected) => {
-  expect(maxSumDistantNumbers(array)).toEqual(expected);
+test.each(couples)('largestSumOfNonAdjacent', (array, expected) => {
+  expect(largestSumOfNonAdjacent(array)).toEqual(expected);
 });
